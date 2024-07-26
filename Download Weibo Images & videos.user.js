5f3fb31e3ae9d415214555ebf37f9d57eaae8e21
owendswang <9076865+owendswang@users.noreply.github.com>
2024-07-27 04:03:37 +0800

    Update Download Weibo Images & videos.user.js

--- Download Weibo Images & videos.user.js
+++ Download Weibo Images & videos.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Download Weibo Images & Videos (Only support new version weibo UI)
 // @name:zh-CN   下载微博图片和视频（仅支持新版界面）
-// @version      1.1.9
+// @version      1.2.0
 // @description  Download images and videos from new version weibo UI webpage.
 // @description:zh-CN 从新版微博界面下载图片和视频。
 // @author       OWENDSWANG
